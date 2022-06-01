b11796ab8ba29191cb66c19d59ff539b6b17f5eb
Bharath <bharathsurya021@gmail.com>
2022-06-01 07:40:45 +0530

    Jwt,protected routes,login/register/logout

---
+++ server/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
+
+const userSchema = new mongoose.Schema({
+	name: { type: String, required: true },
+	email: { type: String, required: true },
+	phone: { type: Number, required: true },
+	password: { type: String, required: true },
+});
+
+userSchema.methods.generateAuthToken = function () {
+	const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
+		expiresIn: "7d",
+	});
+	return token;
+};
+
+const User = mongoose.model("user", userSchema);
+
+const validate = (data) => {
+	const schema = Joi.object({
+		name: Joi.string().required().label("Name"),
+		email: Joi.string().email().required().label("Email"),
+		phone: Joi.number().integer().min(1).max(9999999999).label("Phone"),
+		password: passwordComplexity().required().label("Password"),
+	});
+	return schema.validate(data);
+};
+
+module.exports = { User, validate };
